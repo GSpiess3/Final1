16b23ded94abbbe677d7035a66ab68cdfbf1517c
GSpiess3 <gabespiess@gmail.com>
2023-05-01 20:59:01 -0500

    Second Commit

--- gui.py
+++ gui.py
@@ -5,20 +5,24 @@ class GUI:
         self.window = window
 
         # Radio buttons
-        self.frame_shape = Frame(self.window)
-        self.label_Options = Label(self.frame_shape, text='Shape\t')
+        self.frame_Select = Frame(self.window)
+        self.label_Options = Label(self.frame_Select, text='Select One of the options below')
         self.radio_1 = IntVar()
         self.radio_1.set(0)
-        self.radio_add = Radiobutton(self.frame_shape, text='Add', variable=self.radio_1, value=1, command=self.shape)
-        self.radio_sub = Radiobutton(self.frame_shape, text='Sub', variable=self.radio_1, value=2, command=self.shape)
-        self.radio_mult = Radiobutton(self.frame_shape, text='Multiplication', variable=self.radio_1, value=3, command=self.shape)
-        self.radio_div = Radiobutton(self.frame_shape, text='Divide', variable=self.radio_1, value=4, command=self.shape)
+        self.radio_add = Radiobutton(self.frame_Select, text='Add', variable=self.radio_1, value=1, command=self.Option)
+        self.radio_sub = Radiobutton(self.frame_Select, text='Sub', variable=self.radio_1, value=2, command=self.Option)
+        self.radio_mult = Radiobutton(self.frame_Select, text='Multiplication', variable=self.radio_1, value=3, command=self.Option)
+        self.radio_div = Radiobutton(self.frame_Select, text='Divide', variable=self.radio_1, value=4, command=self.Option)
         self.label_Options.pack(padx=5)
-        self.radio_add.pack(side='left')
-        self.radio_sub.pack(side='left')
-        self.radio_mult.pack(side='left')
-        self.radio_div.pack(side='left')
-        self.frame_shape.pack(anchor='w', pady=10)
+        self.radio_add.pack()
+        self.radio_sub.pack()
+        self.radio_mult.pack()
+        self.radio_div.pack()
+        self.frame_Select.pack(anchor='w', pady=10)
+        
+        # Example Label
+        self.label_example =
+        
 
         # First number
         self.frame_first = Frame(self.window)
@@ -46,7 +50,7 @@ class GUI:
 
         # Compute button
         self.frame_button = Frame(self.window)
-        self.button_compute = Button(self.frame_button, text='COMPUTE', command=self.compute)
+        self.button_compute = Button(self.frame_button, text='COMPUTE', command=self.Compute)
         self.button_compute.pack(pady=10)
         self.frame_button.pack()
 
@@ -74,7 +78,7 @@ class GUI:
             self.label_second.config(text='Second Number')
             self.entry_second.pack()
 
-    def compute(self):
+    def Compute(self):
         try:
             first_num = self.entry_first.get()
             second_num = self.entry_second.get()
